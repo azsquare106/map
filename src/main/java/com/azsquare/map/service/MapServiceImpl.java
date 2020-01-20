36afbefc4cdf839b3667c9ebf8bf02f4bf7a0ba8
azsquare106 <59550218+azsquare106@users.noreply.github.com>
2020-01-20 15:01:49 -0600

    Create MapServiceImpl.java

---
+++ src/main/java/com/azsquare/map/service/MapServiceImpl.java
@@ -0,0 +1,16 @@
+package com.azsquare.map.service;
+
+import com.azsquare.map.model.User;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+
+@Service
+public class MapServiceImpl implements MapService {
+
+    @Override
+    public List<User> getUsers() {
+
+        return null;
+    }
+}
