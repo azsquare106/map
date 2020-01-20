46cec074d2a69ef14fcce0cff10a23ec888c71de
azsquare106 <59550218+azsquare106@users.noreply.github.com>
2020-01-20 14:59:42 -0600

    Create User.java

---
+++ src/main/java/com/azsquare/map/model/User.java
@@ -0,0 +1,42 @@
+package com.azsquare.map.model;
+
+public class User {
+
+    private String name;
+    private String lastName;
+    private String type;
+    private int numberOfPlaces;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getNumberOfPlaces() {
+        return numberOfPlaces;
+    }
+
+    public void setNumberOfPlaces(int numberOfPlaces) {
+        this.numberOfPlaces = numberOfPlaces;
+    }
+}
