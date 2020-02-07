715bd3389f37ce518fd5a5fcf13663139d0df372
azsquare106 <59550218+azsquare106@users.noreply.github.com>
2020-02-07 15:52:56 -0600

    Create PaymentController.java

---
+++ src/main/java/com/azsquare/map/controller/PaymentController.java
@@ -0,0 +1,62 @@
+package com.azsquare.buy.controller;
+
+import com.azsquare.buy.Service.StripeService;
+import com.azsquare.buy.model.ChargeRequest;
+import com.stripe.exception.StripeException;
+import com.stripe.model.Charge;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class PaymentController {
+
+
+
+
+    @Autowired
+    private StripeService stripeService;
+
+
+
+    @Value("${STRIPE_PUBLIC_KEY}")
+    private String stripePublicKey;
+
+    @RequestMapping("/")
+    public String checkout(Model model) {
+        model.addAttribute("amount", 50 * 100); // in cents
+        model.addAttribute("stripePublicKey", stripePublicKey);
+        model.addAttribute("currency", ChargeRequest.Currency.EUR);
+        return "index";
+    }
+
+
+
+
+    @Autowired
+    private StripeService paymentsService;
+
+    @PostMapping("/charge")
+    public String charge(ChargeRequest chargeRequest, Model model)
+            throws StripeException {
+        chargeRequest.setDescription("Example charge");
+        chargeRequest.setCurrency(ChargeRequest.Currency.EUR);
+        Charge charge = paymentsService.charge(chargeRequest);
+        model.addAttribute("id", charge.getId());
+        model.addAttribute("status", charge.getStatus());
+        model.addAttribute("chargeId", charge.getId());
+        model.addAttribute("balance_transaction", charge.getBalanceTransaction());
+        return "result";
+    }
+
+    @ExceptionHandler(StripeException.class)
+    public String handleError(Model model, StripeException ex) {
+        model.addAttribute("error", ex.getMessage());
+        return "result";
+    }
+}
